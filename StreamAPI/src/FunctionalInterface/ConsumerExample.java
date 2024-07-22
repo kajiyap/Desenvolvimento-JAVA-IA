50d04a774cff40ca65286b0c50a991fa3b90f168
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2024-07-22 19:12:53 -0300

    conclusão da aprendizagem do Stream API

--- StreamAPI/src/FunctionalInterface/ConsumerExample.java
+++ StreamAPI/src/FunctionalInterface/ConsumerExample.java
@@ -3,7 +3,11 @@ package FunctionalInterface;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
-
+/**
+ * Representa uma operação que aceita um argumento do tipo T e não retorna nenhum resultado.
+ * É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar, ou
+ * retornar um valor.
+ */
 public class ConsumerExample {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1,2,3,4,5);
